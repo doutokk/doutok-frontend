7e20c764ef07a5cb8ae81903d4eeeec34f93dbc4
suyiiyii <suyiiyii@gmail.com>
2025-02-15 20:00:22 +0800

    feat: update Tailwind CSS integration and remove deprecated configuration files

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import tailwind from '@tailwindcss/postcss'
-import autoprefixer from 'autoprefixer'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import tailwindcss from "@tailwindcss/vite";
+import autoprefixer from "autoprefixer";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   css: {
     postcss: {
-      plugins: [
-        tailwind,
-        autoprefixer()
-      ]
-    }
-  }
-})
+      plugins: [autoprefixer()],
+    },
+  },
+});
