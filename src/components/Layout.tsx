c2ec3cc9dd393741206787e2ef77ac5761b0b3c4
suyiiyii <suyiiyii@gmail.com>
2025-02-15 16:38:51 +0800

    test

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,18 +1,44 @@
-import { Link, Outlet } from 'react-router-dom';
+import React from "react";
+import { Link, Outlet } from "react-router-dom";
+import { Layout as AntLayout, Menu, Input } from "antd";
+import { ShoppingCartOutlined } from "@ant-design/icons";
+
+const { Header, Content, Footer } = AntLayout;
 
 const Layout = () => {
   return (
-    <div>
-      <nav>
-        <ul>
-          <li><Link to="/">首页</Link></li>
-          <li><Link to="/about">关于</Link></li>
-        </ul>
-      </nav>
-      <main>
+    <AntLayout style={{ minHeight: "100vh", minWidth: 1200 }}>
+      <Header style={{ display: "flex", alignItems: "center", justifyContent: "space-between", padding: "0 20px" }}>
+        <div style={{ flexShrink: 0 }}>
+          <Menu mode="horizontal" style={{ borderBottom: "none" }} selectedKeys={[]}>
+            <Menu.SubMenu key="sub1" title="分类">
+              <Menu.Item key="1">分类1</Menu.Item>
+              <Menu.Item key="2">分类2</Menu.Item>
+            </Menu.SubMenu>
+            <Menu.Item key="about">
+              <Link to="/about">about</Link>
+            </Menu.Item>
+          </Menu>
+        </div>
+        <div>
+          <Input.Search
+            placeholder="搜索"
+            style={{ width: 200 }}
+            onSearch={(value) => console.log(value)}
+          />
+        </div>
+        <div style={{ flexShrink: 0 }}>
+          <Menu mode="horizontal" style={{ borderBottom: "none" }} selectedKeys={[]}>
+            <Menu.Item key="login">登录</Menu.Item>
+            <Menu.Item key="cart" icon={<ShoppingCartOutlined />} />
+          </Menu>
+        </div>
+      </Header>
+      <Content style={{ padding: "0 50px", marginTop: 24 }}>
         <Outlet />
-      </main>
-    </div>
+      </Content>
+      <Footer style={{ textAlign: "center" }}>© 2025 Doutok Team</Footer>
+    </AntLayout>
   );
 };
 
